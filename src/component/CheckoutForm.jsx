90297a6685130a236aab5bb67678415590470146
Sam <sam.mamane@hotmail.fr>
2019-09-27 15:13:10 +0200

    Test ultime

--- src/component/CheckoutForm.jsx
+++ src/component/CheckoutForm.jsx
@@ -10,15 +10,22 @@ class CheckoutForm extends Component {
   async submit(ev) {
     let token = await this.props.stripe.createToken({name: "Name"});
     console.log(token)
-
-    let {status} = await this.props.stripe.charges.create({
-      amount: 2000,
-      currency: "usd",
-      description: "An example charge",
-      source: token.id
+    console.log(this.props)
+    try {
+        let status = await this.props.stripe.paymentRequest({
+      country: 'US',
+      currency: 'usd',
+      total: {
+        label: 'Demo total',
+        amount: 1000,
+      },
     });
 
-    if (response.ok) console.log("Purchase Complete!")
+        console.log(status)
+      } catch (err) {
+        console.log(err)
+    }
+
   }
 
   render() {
